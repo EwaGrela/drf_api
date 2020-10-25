913f1fbad53b74af329ce4806257254b8d9e9864
EwaGrela <ewa_trojanowska@wp.pl>
2020-10-25 21:05:01 +0100

    readd debug

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
 ALLOWED_HOSTS = [
     '127.0.0.1',
     '.herokuapp.com',
