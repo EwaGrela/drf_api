d1bd5dd355c0e9c5da4832f80b5224231cea06b9
EwaGrela <ewa_trojanowska@wp.pl>
2020-10-25 20:59:29 +0100

    add static config

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -128,6 +128,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    ("js", os.path.join(STATIC_ROOT,'js')),
+    ("css", os.path.join(STATIC_ROOT,'css')),
+    ("images", os.path.join(STATIC_ROOT,'images')),
+    ("fonts", os.path.join(STATIC_ROOT,'fonts')),
+)
+
 STATIC_URL = '/static/'
 
 
